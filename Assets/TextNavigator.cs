176d3d7d0ecb3b0319f0bb9d17635babe27f170b
Charles <cwolfe7@asu.edu>
2021-05-31 09:53:11 -0700

    text navigator prefab complete

---
+++ Assets/TextNavigator.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextNavigator : MonoBehaviour
+{
+    [SerializeField] Text[] dialogueTexts;
+    private Text dialogueText;
+    private int dialogueTextIdx;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dialogueTextIdx = 0;
+        SetActiveText(dialogueTextIdx);
+        dialogueText = dialogueTexts[dialogueTextIdx];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void NextText()
+    {
+        dialogueTextIdx += 1;
+        SetActiveText(dialogueTextIdx);
+    }
+
+    private void SetActiveText(int idx)
+    {
+        if (idx >= dialogueTexts.Length) { return; }
+
+        foreach (Text text in dialogueTexts)
+        {
+            text.gameObject.SetActive(false);
+        }
+
+        dialogueTexts[idx].gameObject.SetActive(true);
+    }
+}
