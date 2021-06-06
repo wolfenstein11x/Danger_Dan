a7d92eb1985445df01e926dd3bdb5f0b49030b13
Charles <cwolfe7@asu.edu>
2021-06-05 22:55:28 -0700

    player can jump now

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float runSpeed = 1.0f;
+    [SerializeField] float jumpSpeed = 28f;
+
+    Rigidbody2D rigidBody;
+    Animator animator;
+    Collider2D playerCollider;
+
+    float epsilon = 0.0001f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        playerCollider = GetComponent<Collider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Run();
+        FlipSprite();
+        Jump();
+    }
+
+    private void Run()
+    {
+        float controlThrow = Input.GetAxis("Horizontal");   // -1 to +1
+
+        Vector2 playerVelocity = new Vector2(controlThrow * runSpeed, rigidBody.velocity.y);
+
+        rigidBody.velocity = playerVelocity;
+
+        bool movingHorizontal = Mathf.Abs(rigidBody.velocity.x) > Mathf.Epsilon 
+                                && Mathf.Abs(rigidBody.velocity.y) < epsilon;
+        
+        animator.SetBool("running", movingHorizontal);
+
+    }
+
+    private void Jump()
+    {
+        if (!playerCollider.IsTouchingLayers(LayerMask.GetMask("Ground"))) { return; }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            Vector2 jumpVelocityToAdd = new Vector2(0f, jumpSpeed);
+            rigidBody.velocity += jumpVelocityToAdd;
+        }
+
+        bool jumping = Mathf.Abs(rigidBody.velocity.y) > epsilon;
+        animator.SetBool("jumping", jumping);
+        
+    }
+
+    private void FlipSprite()
+    {
+        bool movingHorizontal = Mathf.Abs(rigidBody.velocity.x) > Mathf.Epsilon;
+
+        if (movingHorizontal)
+        {
+            transform.localScale = new Vector2(Mathf.Sign(rigidBody.velocity.x), 1f);
+        }
+    }
+}
