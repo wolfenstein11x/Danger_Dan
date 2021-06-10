5b57674f0d84a8fe5bd905de2c6e153b3a153a42
Charles <cwolfe7@asu.edu>
2021-06-10 15:34:02 -0700

    enemy prefab moves back and forth

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 1f;
+
+    Rigidbody2D rigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (IsFacingNormally())
+        {
+            rigidBody.velocity = new Vector2(-moveSpeed, 0f);
+        }
+
+        else
+        {
+            rigidBody.velocity = new Vector2(moveSpeed, 0f);
+        }
+        
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        transform.localScale = new Vector2(Mathf.Sign(rigidBody.velocity.x), 1f);
+    }
+
+    private bool IsFacingNormally()
+    {
+        return transform.localScale.x > 0;
+    }
+}
